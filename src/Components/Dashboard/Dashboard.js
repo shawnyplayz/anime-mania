f49e206f743dee340dbae919eeae864a8a2bfd94
Shawn Almeida <55237616+shawnyplayz@users.noreply.github.com>
2022-09-16 15:03:06 +0530

    created cards

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -7,69 +7,18 @@ export default class Dashboard extends Component {
   render() {
     return (
       <>
-        <div className="row">
-          {/* <div className="col-lg-2">
-            <div class="page-wrapper chiller-theme toggled">
-              <a id="show-sidebar" className="btn btn-sm btn-dark" href="#">
-                <i className="fas fa-bars"></i>
-              </a>
-              <nav id="sidebar" className="sidebar-wrapper">
-                <div className="sidebar-content">
-                  <div className="sidebar-header">
-                    <div className="user-pic">
-                      <img className="img-responsive img-rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIFXiJ7c2zSE67AlROJYCnk46mggZwfmts5BFy3sTbPg&s.jpg"
-                        alt="User picture" />
-                    </div>
-                    <div className="user-info">
-                      <span className="user-name">
-                        <strong>Shawn Almeida</strong>
-                      </span>
-                      <span className="user-role">Administrator</span>
-                      <span className="user-status">
-                        <i className="fa fa-circle"></i>
-                        <span>Online</span>
-                      </span>
-                    </div>
-                  </div>
-                  <div className="sidebar-menu">
-                    <ul>
-                      <li>
-                        <NavLink to='/Home'>
-                          <i className="fa fa-folder"></i>
-                          <span>Home</span>
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink to='/Bookmark'>
-                          <i className="fa fa-folder"></i>
-                          <span>Bookmarks</span>
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink to='/'>
-                          <i className="fa fa-folder"></i>
-                          <span>Log Out</span>
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-            </div>
-          </div> */}
-          <div className="col-lg-12">
-            <Switch>
-              <Route exact path="/Dashboard"  >
-                <Home />
-              </Route>
-              <Route path="/Home"  >
-                <Home />
-              </Route>
-              <Route path="/Bookmark"  >
-                <Bookmark />
-              </Route>
-            </Switch>
-          </div>
+        <div className="container-fluid">
+          <Switch>
+            <Route exact path="/Dashboard"  >
+              <Home />
+            </Route>
+            <Route path="/Home"  >
+              <Home />
+            </Route>
+            <Route path="/Bookmark"  >
+              <Bookmark />
+            </Route>
+          </Switch>
         </div >
       </>
     )
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,8 @@ import { bookmark } from "../../Reducer/actions";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./Home.css"
-import { AiOutlineSearch } from 'react-icons/ai';
+import { AiOutlineArrowRight, AiOutlineSearch } from 'react-icons/ai';
+import { BsFillHeartFill } from 'react-icons/bs';
 
 class Home extends Component {
   constructor(props) {
@@ -41,7 +42,7 @@ orites&sort=desc`)
 
   render() {
     return (
-      <div className="container-fluid p-0">
+      <div className="container p-0">
         <h1 className="display-3 my-head">Search Anime Characters</h1>
         <div className="row">
           <div className="row justify-content-center">
@@ -57,11 +58,8 @@ orites&sort=desc`)
               </div>
             </div>
           </div>
-          <div className="row justify-content-center">
-            <div className="col-lg-6 col-md-6 col-sm-12">
-              <h5>Total {this.state.animeLength} matching anime Characters found</h5>
-            </div>
-          </div>
+
+
           {/* {this.state.animeArr.map((el, index) => {
             if (el.type === 'story' && el.url)
               return (
@@ -106,13 +104,65 @@ orites&sort=desc`)
               );
           })} */}
         </div>
-        <div className="row p-0">
-          <div className="card mt-4">
-
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <h5>Total {this.state.animeLength} matching anime Characters found</h5>
           </div>
         </div>
+        <div className="row">
+          <div className="row justify-content-center">
+            <div className="col-lg-12 opaque">
+              <div className=" p-2 mt-4">
+                {
+                  this.state.animeArr.map((el, index) => {
+                    debugger
+                    return (
+                      <div className="card my-2 shadow "
+                        key={index}>
+                        <div className="d-flex flex-row">
+                          <div className="img-fluid border">
+                            <img className="card-img" src={el.images.jpg.image_url} alt="Card image cap"></img>
+                          </div>
+                          <div className="card-Header d-flex justify-content-start w-75 pl-3 border">
+                            <h3>{el.name}</h3>
+                            {el.nicknames.map(item =>
+                              <button className="btn">
+                                {item}
+                              </button>
+                            )}
+                          </div>
+                          <div className="card-Text d-flex justify-content-around border">
+                            <h4><BsFillHeartFill />{el.favorites}</h4>
+                          </div>
+                          <div className="d-flex justify-content-center">
+                            <h1><AiOutlineArrowRight /></h1>
+                          </div>
+                        </div>
+                        {/* <div className="d-flex  justify-content-start border">
+                          <div className="img-fluid ">
+                            <img className="card-img" src={el.images.jpg.image_url} alt="Card image cap"></img>
+                          </div>
+                        </div>
+                        <div className="card-Header justify-content-left">
+                          {el.name}
+                        </div>
+                        <div className="card-Body">
+                          {el.favorites}
+                        </div> */}
+                      </div>
+
+                    )
+                  }
+                  )
+                }
+              </div>
+            </div>
+          </div>
+
+
+        </div>
         <ToastContainer />
-      </div>
+      </div >
     );
   }
 }
